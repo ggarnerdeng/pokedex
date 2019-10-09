712ae14f6d484cca6313403f5c5202efc0073f36
ggarnerdeng <ggarnerdeng@gmail.com>
2019-10-09 17:00:57 -0500

    "made core"

--- pokemon.go
+++ pokemon.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 //Pokemon is a data struct representing each poke we caught or seen
 type Pokemon struct {
 	id     int
 	name   string
 	caught bool
+}
+
+//Create constructs a pokemon
+func (p *Pokemon) Create(id int, name string, caught bool, pokedex []Pokemon) {
+	newPokemon := Pokemon{
+		id:     id,
+		name:   name,
+		caught: caught,
+	}
+	pokedex = append(pokedex, newPokemon)
+}
+
+//Read prints pokedex
+func (p *Pokemon) Read(pokedex []Pokemon) {
+	for _, p := range pokedex {
+		fmt.Println(p.name, p.caught)
+	}
 }
