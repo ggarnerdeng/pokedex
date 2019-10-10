da101aebb1ac4db0699a1c76442f0612e2d4a6ff
ggarnerdeng <ggarnerdeng@gmail.com>
2019-10-10 14:37:30 -0500

    "html stuff

--- main.go
+++ main.go
@@ -2,14 +2,36 @@ package main
 
 //var pokedex []Pokemon
 import (
+	"flag"
+	"net/http"
+	_ "net/http"
 	"os"
 	"strconv"
 )
 
+var pokedex Pokedex
+
+func addPokemon(res http.ResponseWriter, req *http.Request) {
+	id, _ := strconv.Atoi(req.FormValue("id"))
+	name := req.FormValue("name")
+	var caught bool
+	//if req.FormValue("caught") = "true" {
+	//	caught = true
+	//}
+	//caught := req.FormValue("caught")
+	httpPokemon := NewPokemon(id, name, caught)
+	pokedex.Add(httpPokemon)
+	pokedex.List()
+}
 func main() {
 	pokedex := Pokedex{}
 	bulbasaur := NewPokemon(1, "Bulbasaur", true)
 	pokedex.Add(bulbasaur)
+
+	httpport := flag.String("http", ":8080", "port value")
+	flag.Parse()
+	http.HandleFunc("/pokemon", addPokemon)
+	http.ListenAndServe(*httpport, nil)
 
 	if len(os.Args) > 1 {
 		id, _ := strconv.Atoi(os.Args[1])
